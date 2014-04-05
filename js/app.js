93fb8228fe347f3b3c92e890350123013f24fde8
Colin Manning <colin.manning@mac.com>
2014-04-05 22:08:49 +0200

    added root category

--- js/app.js
+++ js/app.js
@@ -2,5 +2,6 @@
 
 var app = angular.module('disBrowser',['angularFileUpload', 'ui.bootstrap']);
 app.baseUrl = "http://dis.berlinirish.com";
+app.rootCategory = { id: 1, path: "$Categories"}
 
 })();
